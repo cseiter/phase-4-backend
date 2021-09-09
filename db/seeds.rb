61e523e6e94367761bc403f052b23c0e23aa6c9a
chris <cseiter@gmail.com>
2021-09-09 18:45:56 -0400

    update seed file to delete existing data from room table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Room.delete_all
+
 Room.create!(room: "Living Room")
 Room.create!(room: "Kitchen")
 Room.create!(room: "Office")
