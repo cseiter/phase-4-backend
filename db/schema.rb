036a53dde5f5defbab8fe6451c96dba063588a04
chris <cseiter@gmail.com>
2021-09-10 18:40:12 -0400

    ran category index migration, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_233449) do
+ActiveRecord::Schema.define(version: 2021_09_10_222528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,22 @@ ActiveRecord::Schema.define(version: 2021_09_09_233449) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "item_name"
+    t.string "manufacturer"
+    t.string "model"
+    t.string "serial_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "room"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "categories"
 end
