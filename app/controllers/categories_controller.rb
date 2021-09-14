5b3ba5dcc7ec531407b18af6f0a3f86678ba9868
chris <cseiter@gmail.com>
2021-09-14 14:21:26 -0400

    categories_controller updated with show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,8 @@ class CategoriesController < ApplicationController
     def index
         render json: Category.all
     end
+
+    def show
+        render json: Category.find(params[:id])
+    end
 end
