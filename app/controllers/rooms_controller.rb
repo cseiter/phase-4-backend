e5c3c3f3573d6f48c5edbe6751023c0f77f49578
chris <cseiter@gmail.com>
2021-09-16 16:44:48 -0400

    add create route and params to RoomsController

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,13 @@ class RoomsController < ApplicationController
     def show
         render json: Room.find(params[:id])
     end
+
+    def create
+        render json: Room.create!(room_params), status: :created
+    end
+
+    def room_params
+        params.permit(:room)
+    end
+
 end
