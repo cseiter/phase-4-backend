953bc833fbe94a18a63e0e1ae95e64f221410c8a
chris <cseiter@gmail.com>
2021-09-16 20:50:52 -0400

    updated controller for routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,23 @@ class CategoriesController < ApplicationController
     def show
         render json: Category.find(params[:id])
     end
+
+    def create
+        render json: Category.create!(category_params), status: :created
+    end
+
+    def destroy
+        category = Category.find_by(id: params[:id])
+        if category
+          category.destroy
+          head :no_content
+        else
+          render json: { error: "Category not found" }, status: :not_found
+        end
+    end       
+
+    def category_params
+        params.permit(:category)
+    end
+
 end
