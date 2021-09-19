bd4d5565bae4b463a4f8c8f4f5795e3c1897a5e0
chris <cseiter@gmail.com>
2021-09-19 16:32:31 -0400

    complete conditions_controller out

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -3,4 +3,27 @@ class ConditionsController < ApplicationController
     def index
         render json: Condition.all
     end
+
+    def show
+        render json: Condition.find(params[:id])
+    end
+
+    def create
+        render json: Condition.create!(condition_params), status: :created
+    end
+
+    def destroy
+        condition = Condition.find_by(id: params[:id])
+        if condition
+          condition.destroy
+          head :no_content
+        else
+          render json: { error: "Condition not found" }, status: :not_found
+        end
+    end       
+
+    def condition_params
+        params.permit(:condition)
+    end
+
 end
